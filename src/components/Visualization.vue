1c4000cdea943b5de7f4651aed7c461101ba87d6
Paula <eduardseiler23@gmail.com>
2018-11-28 16:01:03 +0100

    show particles on canvas

--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -1,16 +1,58 @@
 <template>
-  <div class="viz"></div>
+    <div class="viz">
+        <canvas id="threeCanvas" ></canvas>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "Visualization"
-};
+    import * as THREE from "three";
+
+    export default {
+        name: "Visualization",
+
+        methods: {
+            init() {
+                let canvas = document.getElementById('threeCanvas');
+                let renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+                console.log(canvas.width);
+                renderer.setSize(canvas.width, canvas.height, false);
+                this.$three.camera.aspect = canvas.width / canvas.height;
+                this.$three.camera.updateProjectionMatrix();
+                var material = new THREE.PointsMaterial({
+                    size: 5,
+                    vertexColors: THREE.VertexColors
+                });
+                var geometry = new THREE.Geometry();
+                var x, y, z;
+                for (var i = 0; i < 1000; i++) {
+                    x = (Math.random() * 800) - 400;
+                    y = (Math.random() * 800) - 400;
+                    z = (Math.random() * 800) - 400;
+
+                    geometry.vertices.push(new THREE.Vector3(x, y, z));
+                    geometry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
+                    geometry.colors[i] = new THREE.Color(Math.random(), Math.random(), Math.random());
+
+                }
+                var pointCloud = new THREE.Points(geometry, material);
+                this.$three.scene.add(pointCloud);
+                renderer.render(this.$three.scene, this.$three.camera);
+            }
+        },
+        mounted() {
+            this.init();
+        }
+    };
 </script>
 
 <style lang="scss">
-.viz {
-  height: 25vh;
-  background-color: #333;
-}
+    .viz {
+        height: 25vh;
+        background-color: #333;
+    }
+
+    #threeCanvas {
+        width: inherit;
+        height: inherit;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as THREE from "three";
 
 Vue.config.productionTip = false;
 
@@ -10,6 +11,12 @@ const gain = audioContext.createGain();
 gain.connect(audioContext.destination);
 
 Vue.prototype.$audio = { audioContext, gain };
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(160, 5, 1, 1000);
+
+
+Vue.prototype.$three = { scene, camera };
 
 new Vue({
   router,
