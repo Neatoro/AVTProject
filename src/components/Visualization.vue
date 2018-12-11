22aabfc8a87e9e9e7d80ebeac96c2e7ddf155e26
Paula <paula.ost123@gmail.com>
2018-12-11 10:12:44 +0100

    one wirecube

--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -8,7 +8,7 @@ import * as THREE from "three";
 export default {
   name: "Visualization",
   data: () => ({
-    wireframe: null
+    mesh: null
   }),
 
   methods: {
@@ -20,25 +20,36 @@ export default {
       });
       this.handleResize();
       let geometry = new THREE.BoxGeometry(50, 50, 50);
-      let mat = new THREE.MeshBasicMaterial({
+      let material = new THREE.MeshPhongMaterial({
         color: 0xfefefe,
         opacity: 0.5
       });
-      this.wireframe = new THREE.Mesh(geometry, mat);
-      this.$three.scene.add(this.wireframe);
-      this.$three.renderer.render(this.$three.scene, this.$three.camera);
-      this.wireframe.position.z = -100;
+      this.mesh = new THREE.Mesh(geometry, material);
+      let geo = new THREE.EdgesGeometry(this.mesh.geometry);
+      let mat = new THREE.LineBasicMaterial({
+          color:0xffffff,
+          linewidth:4
+      });
+      let wireframe = new THREE.LineSegments(geo, mat);
+
+        this.mesh.add(wireframe);
+      this.$three.scene.add(this.mesh);
+        this.mesh.position.z = -100;
+        console.log(this.mesh.position.z);
+        this.$three.renderer.render(this.$three.scene, this.$three.camera);
       this.render.bind(this)();
     },
     render() {
-      window.requestAnimationFrame(this.render);
       this.$audio.analyser.getByteFrequencyData(this.$audio.dataArray);
-      /*this.wireframe.color = new THREE.Color(
-        this.$audio.dataArray[0] / 255,
-        this.$audio.dataArray[20] / 255,
-        this.$audio.dataArray[80] / 255
-      );*/
-      this.$three.renderer.render(this.$three.scene, this.$three.camera);
+        /*this.wireframe.color = new THREE.Color(
+          this.$audio.dataArray[0] / 255,
+          this.$audio.dataArray[20] / 255,
+          this.$audio.dataArray[80] / 255
+        );*/
+      this.mesh.rotation.x +=0.01;
+        this.mesh.rotation.z +=0.01;
+        this.$three.renderer.render(this.$three.scene, this.$three.camera);
+        window.requestAnimationFrame(this.render);
     },
     handleResize() {
       this.$refs.threeCanvas.width = window.outerWidth;
