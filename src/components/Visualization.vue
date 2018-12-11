911bfd8e78455dc1c86e83d2bfeb4926d1b6323c
Paula <paula.ost123@gmail.com>
2018-12-11 11:50:41 +0100

    fixed resize

--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -27,35 +27,35 @@ export default {
       this.mesh = new THREE.Mesh(geometry, material);
       let geo = new THREE.EdgesGeometry(this.mesh.geometry);
       let mat = new THREE.LineBasicMaterial({
-          color:0xffffff,
-          linewidth:4
+        color: 0xffffff,
+        linewidth: 4
       });
       let wireframe = new THREE.LineSegments(geo, mat);
 
-        this.mesh.add(wireframe);
+      this.mesh.add(wireframe);
       this.$three.scene.add(this.mesh);
-        this.mesh.position.z = -100;
-        console.log(this.mesh.position.z);
-        this.$three.renderer.render(this.$three.scene, this.$three.camera);
+      this.mesh.position.z = -100;
+      console.log(this.mesh.position.z);
+      this.$three.renderer.render(this.$three.scene, this.$three.camera);
       this.render.bind(this)();
     },
     render() {
       this.$audio.analyser.getByteFrequencyData(this.$audio.dataArray);
-        /*this.wireframe.color = new THREE.Color(
+      /*this.wireframe.color = new THREE.Color(
           this.$audio.dataArray[0] / 255,
           this.$audio.dataArray[20] / 255,
           this.$audio.dataArray[80] / 255
         );*/
-      this.mesh.rotation.x +=0.01;
-        this.mesh.rotation.z +=0.01;
-        this.$three.renderer.render(this.$three.scene, this.$three.camera);
-        window.requestAnimationFrame(this.render);
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.z += 0.01;
+      this.$three.renderer.render(this.$three.scene, this.$three.camera);
+      window.requestAnimationFrame(this.render);
     },
     handleResize() {
-      this.$refs.threeCanvas.width = window.outerWidth;
+      //this.$refs.threeCanvas.width = window.innerWidth;
       this.$three.renderer.setSize(
-        this.$refs.threeCanvas.width,
-        this.$refs.threeCanvas.height,
+        this.$refs.threeCanvas.clientWidth,
+        this.$refs.threeCanvas.clientHeight,
         false
       );
       this.$three.camera.aspect =
@@ -71,7 +71,7 @@ export default {
 
 <style lang="scss">
 #threeCanvas {
-  width: inherit;
+  width: 100vw;
   height: 25vh;
   background-color: black;
 }
