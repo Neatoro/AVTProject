47da69e7558ebb9c0a25a07ea51796841b507800
eduQu <eduardseiler23@gmail.com>
2018-12-29 01:17:15 +0100

    added 2 presets

--- src/presets.js
+++ src/presets.js
@@ -4,36 +4,53 @@ export default [
     tracks: []
   },
   {
-    name: "Preset 1",
+    name: "bd sd hhc bell",
     tracks: [
       {
         id: 1,
+
+        steps: [
+          true,
+          false,
+          true,
+          false,
+          false,
+          false,
+          false,
+          false,
+          true,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        ]
+      },
+      {
+        id: 2,
         steps: [
           false,
-          true,
+          false,
           false,
           true,
           false,
-          true,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
           false,
           true,
           false,
-          true,
           false,
-          true,
-          false,
-          true,
-          false,
-          true
+          false
         ]
-      }
-    ]
-  },
-  {
-    name: "Preset 2",
-    tracks: [
+      },
       {
-        id: 1,
+        id: 3,
         steps: [
           true,
           false,
@@ -54,24 +71,92 @@ export default [
         ]
       },
       {
+        id: 4,
+        steps: [
+          true,
+          false,
+          true,
+          false,
+          false,
+          false,
+          true,
+          false,
+          true,
+          false,
+          false,
+          false,
+          true,
+          false,
+          true,
+          false
+        ]
+      }
+    ]
+  },
+  {
+    name: "conga conga cow",
+    tracks: [
+      {
+        id: 1,
+        steps: [
+          true,
+          false,
+          false,
+          true,
+          true,
+          false,
+          false,
+          false,
+          true,
+          false,
+          false,
+          true,
+          true,
+          true,
+          false,
+          false
+        ]
+      },
+      {
         id: 2,
         steps: [
           false,
-          true,
           false,
           true,
           false,
-          true,
+          false,
           false,
           true,
           false,
-          true,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false
+        ]
+      },
+      {
+        id: 3,
+        steps: [
+          false,
           false,
           true,
           false,
+          false,
+          false,
           true,
           false,
-          true
+          false,
+          false,
+          true,
+          false,
+          false,
+          false,
+          true,
+          false
         ]
       }
     ]
